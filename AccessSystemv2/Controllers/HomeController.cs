1d142a437f2bd6d466d35a78f5b13481ce7109af
PAULO PAZ <paulopazb@hotmail.com>
2022-05-31 23:56:11 -0400

    Commit Prueba Paulo

--- AccessSystemv2/Controllers/HomeController.cs
+++ AccessSystemv2/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace AccessSystemv2.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            //PRUEBA, BORRAR
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
